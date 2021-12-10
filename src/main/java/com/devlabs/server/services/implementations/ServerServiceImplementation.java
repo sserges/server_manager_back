7463bd5e8290d5156c5e8f56dd0b86daa1866bbd
sserges <agounkeserge@gmail.com>
2021-12-09 20:28:55 -0400

    service impementation 1

--- src/main/java/com/devlabs/server/services/implementations/ServerServiceImplementation.java
+++ src/main/java/com/devlabs/server/services/implementations/ServerServiceImplementation.java
@@ -1,19 +1,44 @@
 package com.devlabs.server.services.implementations;
 
+import com.devlabs.server.enumeration.Status;
 import com.devlabs.server.model.Server;
+import com.devlabs.server.repo.ServerRepo;
 import com.devlabs.server.services.ServerService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.util.Collection;
 
+import static com.devlabs.server.enumeration.Status.SERVER_DOWN;
+import static com.devlabs.server.enumeration.Status.SERVER_UP;
+
+@RequiredArgsConstructor
+@Service
+@Transactional
+@Slf4j
 public class ServerServiceImplementation implements ServerService {
+
+    private final ServerRepo serverRepo;
 
     @Override
     public Server create(Server server) {
-        return null;
+        log.info("Saving new server: {}", server.getName());
+        server.setImageUrl(setServerImageUrl());
+        return serverRepo.save(server);
     }
 
     @Override
-    public Server ping(String ipAddress) {
+    public Server ping(String ipAddress) throws IOException {
+        log.info("Pinging server IP: {}", ipAddress);
+        Server server = serverRepo.findByIpAddress(ipAddress);
+        InetAddress address = InetAddress.getByName(ipAddress);
+        server.setStatus(address.isReachable(10000) ? SERVER_UP : SERVER_DOWN);
+        serverRepo.save(server);
         return null;
     }
 
@@ -34,6 +59,10 @@ public class ServerServiceImplementation implements ServerService {
 
     @Override
     public Boolean delete(Long id) {
+        return null;
+    }
+
+    private String setServerImageUrl() {
         return null;
     }
 }
