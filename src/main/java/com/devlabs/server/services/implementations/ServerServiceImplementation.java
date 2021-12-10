f5b0086ad03ecb78c94ee2d0c15ad494eb73bc98
sserges <agounkeserge@gmail.com>
2021-12-10 12:58:45 -0400

    Service Implementation 3

--- src/main/java/com/devlabs/server/services/implementations/ServerServiceImplementation.java
+++ src/main/java/com/devlabs/server/services/implementations/ServerServiceImplementation.java
@@ -6,16 +6,17 @@ import com.devlabs.server.repo.ServerRepo;
 import com.devlabs.server.services.ServerService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
 import java.io.IOException;
 import java.net.InetAddress;
-import java.net.UnknownHostException;
 import java.util.Collection;
 
 import static com.devlabs.server.enumeration.Status.SERVER_DOWN;
 import static com.devlabs.server.enumeration.Status.SERVER_UP;
+import static java.lang.Boolean.TRUE;
 
 @RequiredArgsConstructor
 @Service
@@ -44,22 +45,27 @@ public class ServerServiceImplementation implements ServerService {
 
     @Override
     public Collection<Server> list(int limit) {
-        return null;
+        log.info("Fetching all servers");
+        return serverRepo.findAll(PageRequest.of(0, limit)).toList();
     }
 
     @Override
     public Server get(Long id) {
-        return null;
+        log.info("Fetching server by id: {}", id);
+        return serverRepo.findById(id).get();
     }
 
     @Override
     public Server update(Server server) {
-        return null;
+        log.info("Updating server: {}", server.getName());
+        return serverRepo.save(server);
     }
 
     @Override
     public Boolean delete(Long id) {
-        return null;
+        log.info("Deleting server by ID: {}", id);
+        serverRepo.deleteById(id);
+        return TRUE;
     }
 
     private String setServerImageUrl() {
