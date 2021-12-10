22d0e33de6a077ae75a18c2a36a090af173b5c7e
sserges <agounkeserge@gmail.com>
2021-12-10 15:56:14 -0400

    service implementation updated

--- src/main/java/com/devlabs/server/services/implementations/ServerServiceImplementation.java
+++ src/main/java/com/devlabs/server/services/implementations/ServerServiceImplementation.java
@@ -8,11 +8,13 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import javax.transaction.Transactional;
 import java.io.IOException;
 import java.net.InetAddress;
 import java.util.Collection;
+import java.util.Random;
 
 import static com.devlabs.server.enumeration.Status.SERVER_DOWN;
 import static com.devlabs.server.enumeration.Status.SERVER_UP;
@@ -69,6 +71,7 @@ public class ServerServiceImplementation implements ServerService {
     }
 
     private String setServerImageUrl() {
-        return null;
+        String[] imageNames = {"server1.png", "server2.png", "server3.png", "server4.png"};
+        return ServletUriComponentsBuilder.fromCurrentContextPath().path("/server/image/" + imageNames[new Random().nextInt(4)]).toUriString();
     }
 }
