ecd7e4983ebb1a56696b47d97f4a1577c68555b3
sserges <agounkeserge@gmail.com>
2021-12-20 11:42:56 -0500

    saveServer & pingServer methods added

--- src/main/java/com/devlabs/server/resources/ServerResource.java
+++ src/main/java/com/devlabs/server/resources/ServerResource.java
@@ -1,17 +1,21 @@
 package com.devlabs.server.resources;
 
+import com.devlabs.server.enumeration.Status;
 import com.devlabs.server.model.Response;
+import com.devlabs.server.model.Server;
 import com.devlabs.server.services.implementations.ServerServiceImplementation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
+import java.io.IOException;
 import java.time.LocalDateTime;
 import java.util.Map;
 
+import static com.devlabs.server.enumeration.Status.SERVER_UP;
+import static org.springframework.http.HttpStatus.CREATED;
 import static org.springframework.http.HttpStatus.OK;
 
 @RestController
@@ -29,6 +33,34 @@ public class ServerResource {
                         .message("Servers retrieved")
                         .status(OK)
                         .statusCode(OK.value())
+                        .build()
+        );
+    }
+
+    @GetMapping("/ping/{ipAddress}")
+    public ResponseEntity<Response> pingServer(@PathVariable("ipAddress") String ipAddress) throws IOException {
+        Server server = serverService.ping(ipAddress);
+        return ResponseEntity.ok(
+                Response.builder()
+                        .timeStamp(LocalDateTime.now())
+                        .data(Map.of("server", server))
+                        .message(server.getStatus() == SERVER_UP ? "Ping success" : "Ping failed")
+                        .status(OK)
+                        .statusCode(OK.value())
+                        .build()
+        );
+    }
+
+    @PostMapping("/save")
+    public ResponseEntity<Response> saveServer(@RequestBody @Valid Server server) {
+
+        return ResponseEntity.ok(
+                Response.builder()
+                        .timeStamp(LocalDateTime.now())
+                        .data(Map.of("server", serverService.create(server)))
+                        .message("Server created")
+                        .status(CREATED)
+                        .statusCode(CREATED.value())
                         .build()
         );
     }
